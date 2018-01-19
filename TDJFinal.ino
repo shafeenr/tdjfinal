ea07d7eab3348a03d6c5aa5355380d64790f0744
Shafeen Rahman <shafeenr@tuta.io>
2018-01-18 22:35:57 -0500

    Initial commit

---
+++ TDJFinal.ino
@@ -0,0 +1,82 @@
+#include <AFMotor.h>
+
+class LED {
+    int ledPin;
+    int ledState;
+    long onTime;
+    long offTime;
+
+    unsigned long previousMillis;
+
+  public:
+    LED (int pin, long on, long off) {
+      ledPin = pin;
+      pinMode(ledPin, OUTPUT);
+
+      onTime = on;
+      offTime = off;
+
+      ledState = LOW;
+      previousMillis = 0;
+    }
+
+    void Update() {
+      unsigned long currentMillis = millis();
+
+      if ((ledState == HIGH) && (currentMillis - previousMillis >= onTime))
+      {
+        ledState = LOW;
+        previousMillis = currentMillis;
+        digitalWrite(ledPin, ledState);
+      }
+      else if ((ledState == LOW) && (currentMillis - previousMillis >= offTime))
+      {
+        ledState = HIGH;
+        previousMillis = currentMillis;
+        digitalWrite(ledPin, ledState);
+      }
+    }
+
+    void ledOff() {
+      digitalWrite(ledState, LOW);
+    }
+};
+
+LED railWarnings(13, 350, 50);
+LED ferrisWheelLights(5, 350, 10);
+
+AF_DCMotor railArmOne(1, MOTOR12_64KHZ);
+AF_DCMotor railArmTwo(2, MOTOR12_64KHZ);
+AF_DCMotor ferrisWheel(3);
+
+void setup() {}
+
+void loop() {
+  railArmOne.setSpeed(255);
+  railArmTwo.setSpeed(255);
+  railArmOne.run(BACKWARD);
+  railArmTwo.run(FORWARD);
+  railWarnings.Update();
+  delay(1500);
+  railArmOne.run(RELEASE);
+  railArmTwo.run(RELEASE);
+
+  ferrisWheel.setSpeed(255);
+  ferrisWheel.run(FORWARD);
+  ferrisWheelLights.Update();
+  railWarnings.Update();
+  delay(5000);
+  ferrisWheel.setSpeed(0);
+  ferrisWheel.run(RELEASE);
+  ferrisWheelLights.ledOff();
+  railWarnings.ledOff();
+
+  railArmOne.setSpeed(255);
+  railArmTwo.setSpeed(255);
+  railArmOne.run(FORWARD);
+  railArmTwo.run(BACKWARD);
+  delay(1900);
+  railArmOne.run(RELEASE);
+  railArmTwo.run(RELEASE);
+  delay(5000);
+}
